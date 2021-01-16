2dbe866688a0cfeaac9a5cb90d538c333db10dde
Sma-source <rabhi.smain@gmail.com>
2021-01-16 18:06:24 +0100

    add: map to transactions

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -7,9 +7,14 @@ export const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span> <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => {
+          return (
+            <li className="minus">
+              {transaction.text} <span>-$400</span>{" "}
+              <button className="delete-btn">x</button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
