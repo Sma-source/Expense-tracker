f88fbc9c0c878d3d0ca2cb68bb9a629ae65626c1
Sma-source <rabhi.smain@gmail.com>
2021-01-16 17:26:03 +0100

    create: provider component

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -14,3 +14,13 @@ const initialState = {
 // Create Context
 
 export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
