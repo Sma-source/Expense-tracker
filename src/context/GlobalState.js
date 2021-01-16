c0876bf1b9849fe82108fc029490f86aab39cccf
Sma-source <rabhi.smain@gmail.com>
2021-01-16 17:19:32 +0100

    init: state with dummies data

---
+++ src/context/GlobalState.js
@@ -0,0 +1,12 @@
+import React, { createContext, useReducer } from "react";
+
+// Init state
+
+const initialState = {
+  transactions: [
+    { id: 1, text: "Book", amount: -15 },
+    { id: 2, text: "Gloves", amount: -10 },
+    { id: 3, text: "Netflix", amount: -11 },
+    { id: 4, text: "Amazon Prime", amount: -5.99 },
+  ],
+};
