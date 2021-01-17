dadeb5d93938685c8b335e931a1c2272b86afb5d
Sma-source <rabhi.smain@gmail.com>
2021-01-17 20:12:56 +0100

    import: useContext and deleteTransaction

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
   const sign = transaction.amount < 0 ? "-" : "+";
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
