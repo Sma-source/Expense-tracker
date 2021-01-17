7c527bf710299287f7267f66f9c9c44d0313137c
Sma-source <rabhi.smain@gmail.com>
2021-01-17 20:03:22 +0100

    change to fixed method by *100/100

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -5,24 +5,29 @@ export const IncomeExpenses = () => {
   const { transactions } = useContext(GlobalContext);
   const amounts = transactions.map((transaction) => transaction.amount);
 
-  const income = amounts
-    .filter((item) => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
+  const income =
+    (amounts
+      .filter((item) => item > 0)
+      .reduce((acc, item) => (acc += item), 0) *
+      100) /
+    100;
 
-  const expense = amounts
-    .filter((item) => item < 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
+  const expense =
+    (amounts
+      .filter((item) => item < 0)
+      .reduce((acc, item) => (acc += item), 0) *
+      -1 *
+      100) /
+    100;
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">${income} </p>
+        <p className="money plus">{income} </p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-$0.00</p>
+        <p className="money minus">{expense.toFixed(2)}</p>
       </div>
     </div>
   );
