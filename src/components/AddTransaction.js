c3c92685f0ab4b0e30eaae353c3b999c46309a20
Sma-source <rabhi.smain@gmail.com>
2021-01-17 20:26:40 +0100

    import: useContext and globalcontext

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
