d418b7a50d60af6b61a9d43956762449736ae4f4
Sma-source <rabhi.smain@gmail.com>
2021-01-17 19:47:43 +0100

    add: total income

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -4,6 +4,11 @@ import { GlobalContext } from "../context/GlobalState";
 export const IncomeExpenses = () => {
   const { transactions } = useContext(GlobalContext);
   const amounts = transactions.map((transaction) => transaction.amount);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
   return (
     <div className="inc-exp-container">
       <div>
