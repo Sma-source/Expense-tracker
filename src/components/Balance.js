dabbf8b89eac62a88aa59388521282ce6cfa98ba
Sma-source <rabhi.smain@gmail.com>
2021-01-17 17:53:27 +0100

    map throw transactions to get all transaction amount

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -3,6 +3,8 @@ import { GlobalContext } from "../context/GlobalState";
 
 export const Balance = () => {
   const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
   return (
     <>
       <h4>Your Balance</h4>
