7b8195a6712d659d187a37338666cca706397c41
Sma-source <rabhi.smain@gmail.com>
2021-01-18 17:38:09 +0100

    add: piechart to incomeexpense component

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Pie } from "react-chartjs-2";
 import { GlobalContext } from "../context/GlobalState";
 
 export const IncomeExpenses = () => {
@@ -19,16 +20,24 @@ export const IncomeExpenses = () => {
       -1 *
       100) /
     100;
+
+  const data = {
+    labels: ["Income", "Expense"],
+    datasets: [
+      {
+        label: "IncomeExpense",
+        data: [income, expense],
+        backgroundColor: ["#2ecc71", "#c0392b"],
+        borderColor: ["rgba(75, 192, 192, 1)", "rgba(255, 99, 132, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
-    <div className="inc-exp-container">
-      <div>
-        <h4>Income</h4>
-        <p className="money plus">{income} </p>
+    <>
+      <div className="inc-exp-container">
+        {income > 0 || expense > 0 ? <Pie data={data} /> : ""}
       </div>
-      <div>
-        <h4>Expense</h4>
-        <p className="money minus">{expense}</p>
-      </div>
-    </div>
+    </>
   );
 };
