93b108720ac86e2cefa9c7baf2286262df5341e9
Sma-source <rabhi.smain@gmail.com>
2021-03-13 14:40:18 +0100

    add dotenv path

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
 
+dotenv.config({ path: "./config/config.env" });
+
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello"));
