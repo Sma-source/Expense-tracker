ea196e9aa38e9d1a63675b067c44bbe4c3f3eb17
Sma-source <rabhi.smain@gmail.com>
2021-03-17 18:24:16 +0100

    bring router file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 
 dotenv.config({ path: "./config/config.env" });
-
+const transactions = require("./routes/transactions");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello"));
