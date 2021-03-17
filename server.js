874e62194ca079f34906981ecf3684123c85b2b4
Sma-source <rabhi.smain@gmail.com>
2021-03-17 18:25:42 +0100

    mount transactions

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config({ path: "./config/config.env" });
 const transactions = require("./routes/transactions");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+app.use("/api/version1/transactions", transactions);
 
 const PORT = process.env.PORT || 5000;
 
