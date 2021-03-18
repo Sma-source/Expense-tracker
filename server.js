067255eb3db6f581cc4948ae0256120946b9bd72
Sma-source <rabhi.smain@gmail.com>
2021-03-18 19:05:12 +0100

    call function to connect to db

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const morgan = require("morgan");
 const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+
 const transactions = require("./routes/transactions");
+
 const app = express();
 
 app.use("/api/version1/transactions", transactions);
