db0ed0038c07d7729f41dd99a6c6595ba78e9e60
Sma-source <rabhi.smain@gmail.com>
2021-03-18 18:17:45 +0100

    method for add transaction

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -4,3 +4,10 @@
 exports.getTransactions = (req, res, next) => {
   res.send("Get Transactions");
 };
+
+// @desc Add transaction
+// @route POST /api/version1/transactions
+// @acces Public
+exports.addTransactions = (req, res, next) => {
+  res.send("Add Transaction");
+};
