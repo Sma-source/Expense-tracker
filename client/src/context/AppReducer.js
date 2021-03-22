f4195142cc3465ed1388194577e1b24fb217a178
Sma-source <rabhi.smain@gmail.com>
2021-03-22 22:52:17 +0100

    add gettransactions action

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -1,5 +1,11 @@
 const AppReducer = (state, action) => {
   switch (action.type) {
+    case "GET_TRANSACTIONS":
+      return {
+        ...state,
+        loading: false,
+        transactions: action.payload,
+      };
     case "DELETE_TRANSACTION":
       return {
         ...state,
