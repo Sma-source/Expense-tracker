a0875757eb9e3945d460a53c158384a2b2f397d8
Sma-source <rabhi.smain@gmail.com>
2021-03-22 23:01:03 +0100

    call getTransactions from globalcontext

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -3,7 +3,7 @@ import { Transaction } from "./Transaction";
 import { GlobalContext } from "../context/GlobalState";
 
 export const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext);
+  const { transactions, getTransactions } = useContext(GlobalContext);
   return (
     <>
       <h3>History</h3>
