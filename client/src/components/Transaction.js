cc2bb260866955c76162b873654feaca44fb2066
Sma-source <rabhi.smain@gmail.com>
2021-03-24 20:39:43 +0100

    add euros symbol

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -11,7 +11,8 @@ export const Transaction = ({ transaction }) => {
       <li className={transaction.amount < 0 ? "minus" : "plus"}>
         {transaction.text}
         <span>
-          {sign}${Math.abs(transaction.amount)}{" "}
+          {sign}
+          {Math.abs(transaction.amount)} €
         </span>{" "}
         <button
           onClick={() => deleteTransaction(transaction._id)}
